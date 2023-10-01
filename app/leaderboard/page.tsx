b8a4c0d99c974f963758f294b7eb4791e7dd5109
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2023-10-01 14:48:37 -0400

    Linked student session totals from the leaderboard to individual pages

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -3,6 +3,7 @@ import PageHeader from "@/app/components/PageHeader";
 import { prisma } from "@/app/lib/prisma";
 import { computeSessionTotals } from "@/app/utils";
 import SessionTotals from "../components/SessionTotals";
+import Link from "next/link";
 
 export const metadata = {
   title: "Brainskills Leaderboard",
@@ -82,18 +83,22 @@ export default async function LeaderboardPage() {
       <h2 className="text-xl lg:text-3xl font-bold text-center mt-10">
         Top 10
       </h2>
-      <ol className="flex flex-col gap-3 py-5">
+      <ol className="flex flex-col gap-4 py-5">
         {studentSessionTotals.slice(0, 10).map((totals, index) => (
-          <li
-            key={totals.student.id}
-            className="flex flex-row items-center mx-auto"
-          >
-            <p className="text-xl font-bold w-6 mx-4 text-right">{index + 1}</p>
-            <SessionTotals
-              totals={totals}
-              displayName={true}
-              className="px-4 py-2"
-            />
+          <li key={totals.student.id} className="mx-auto">
+            <Link
+              href={`/students/${totals.student.id}`}
+              className="flex flex-row items-center"
+            >
+              <p className="text-xl font-bold w-6 mx-4 text-right">
+                {index + 1}
+              </p>
+              <SessionTotals
+                totals={totals}
+                displayName={true}
+                className="px-4 py-2 shadow-md hover:shadow-lg transition duration-200 ease-in-out"
+              />
+            </Link>
           </li>
         ))}
       </ol>
