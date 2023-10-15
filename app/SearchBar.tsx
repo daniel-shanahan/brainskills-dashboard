8a4235f08a610358316d7667c4dbe7d56d54ba1d
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2023-10-15 19:01:05 -0400

    Implemented styling for search bar

--- app/SearchBar.tsx
+++ app/SearchBar.tsx
@@ -1,11 +1,42 @@
-export default async function SearchBar() {
+"use client";
+
+import Select from "react-select";
+import clsx from "clsx";
+import { Student } from "./common.types";
+
+type Props = {
+  students: Student[];
+};
+
+export default function SearchBar({ students }: Props) {
   return (
     <nav className="flex items-center justify-between px-4 py-2">
-      <div className="flex-grow">
-        <input
-          type="text"
-          className="w-3/4 bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-200 px-4 py-2 rounded-md"
+      <div className="flex-grow w3/4">
+        <Select
+          options={students.map((student) => ({
+            value: student.id,
+            label: `${student.firstName} ${student.lastName}`,
+          }))}
+          unstyled
           placeholder="Search for a student..."
+          isClearable
+          noOptionsMessage={() => "No students found"}
+          classNames={{
+            control: ({ isFocused }) =>
+              clsx(
+                "w-3/4 bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-200 px-4 py-2 rounded-md border",
+                isFocused
+                  ? "border-blue-500"
+                  : "border-gray-50 dark:border-gray-950"
+              ),
+            menu: () =>
+              "p-1 mt-2 bg-gray-200 dark:bg-gray-700 rounded border border-white dark:border-gray-800 shadow-md",
+            option: ({ isFocused }) =>
+              clsx(
+                isFocused && "bg-white dark:bg-gray-800",
+                "hover:cursor-pointer px-3 py-2 rounded"
+              ),
+          }}
         />
       </div>
     </nav>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,26 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Sidebar from "./Sidebar";
 import SearchBar from "./SearchBar";
+import { prisma } from "./lib/prisma";
 
 export const metadata: Metadata = {
   title: "BrainSkills Dashboard",
   description: "BrainSkills analytics at a glance",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const students = await prisma.student.findMany();
+
   return (
     <html lang="en">
       <body className="flex bg-gray-50 dark:bg-gray-950 text-gray-950 dark:text-gray-50">
         <Sidebar />
         <main className="flex-grow ml-64 relative">
-          <SearchBar />
+          <SearchBar students={students} />
           {children}
         </main>
       </body>
